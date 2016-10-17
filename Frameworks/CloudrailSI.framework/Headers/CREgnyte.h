68c218b646bc151608a73114187fbf7fd6fc68bb
Felipe Cesar <felipe.cesar@cloudrail.com>
2016-10-17 16:07:10 +0200

    Version 5.0.0 - Swift 3

--- Frameworks/CloudrailSI.framework/Headers/CREgnyte.h
+++ Frameworks/CloudrailSI.framework/Headers/CREgnyte.h
@@ -1,12 +1,8 @@
 
-#import <Foundation/Foundation.h>
 #import "CRCloudStorageProtocol.h"
 
 @interface CREgnyte : NSObject <CRCloudStorageProtocol>
 @property (weak, nonatomic) id target;
 
 -(instancetype)initWithDomain:(NSString *)domain clientId:(NSString *)clientId clientSecret:(NSString *)clientSecret redirectUri:(NSString *)redirectUri state:(NSString *)state;
-
-
-
 @end
